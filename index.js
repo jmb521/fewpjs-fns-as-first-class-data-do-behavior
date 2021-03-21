28df1bcbfa488b58b98c39f5b5871789fac65da3
Jennifer Pazos <jmp521@gmail.com>
2021-03-20 21:51:22 -0500

    commit tree a13ed8e715a9966948dbd880b4a662f76b5b79b1

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(time) {
+  let hour = parseInt(time.split(":")[0])
+  console.log(hour)
+  if(hour < 12) {
+    return "Good Morning"
+  }else if (hour > 12 && hour < 17) {
+    return "Good Afternoon"
+  } else if (hour > 17) {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(greeting) {
+  let showGreeting = document.getElementById("greeting")
+  showGreeting.innerText = greeting
+}
